623049c9fc335c3626b74449a6c5c86532885b9b
Greg Smith <smr.kyu@gmail.com>
2019-12-02 12:40:46 -0600

    2019 Day 1

---
+++ 2019/01b.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const raw = fs.readFileSync('01.txt', 'utf-8');
+const masses = raw.trim().split('\n').map(x => +x);
+const fuels = masses.map(massToFuel);
+const fuel = fuels.reduce((a, b) => a + b, 0);
+
+function massToFuel(mass) {
+	const fuel = Math.floor(mass/3) - 2;
+	if (fuel <= 0) return 0;
+	return fuel + massToFuel(fuel);
+}
+
+console.log(fuel);
