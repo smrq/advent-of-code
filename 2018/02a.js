aa46b8d9a45edc1e92a3a1446e03c6071b752452
Greg Smith <smr.kyu@gmail.com>
2019-12-02 12:58:10 -0600

    2018 Day 2

---
+++ 2018/02a.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const ids = fs.readFileSync('02.txt', 'utf-8')
+	.trim()
+	.split('\n');
+
+const twos = ids.map(id => check(id, 2)).filter(x => x).length;
+const threes = ids.map(id => check(id, 3)).filter(x => x).length;
+console.log(twos * threes);
+
+function check(id, n) {
+	const counts = countLetters(id);
+	return Object.values(counts).includes(n);
+}
+
+function countLetters(str) {
+	const result = {};
+	str.split('').forEach(letter => {
+		result[letter] = (result[letter] || 0) + 1;
+	});
+	return result;
+}
