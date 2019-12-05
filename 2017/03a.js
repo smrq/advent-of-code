5bb527172b37422df639344d63fa896fee4c731d
Greg Smith <smr.kyu@gmail.com>
2019-12-05 17:14:40 -0600

    2017 Day 3

---
+++ 2017/03a.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const fs = require('fs');
+const input = +fs.readFileSync('03.txt', 'utf-8').trim();
+const tests = [
+	[1, 0],
+	[12, 3],
+	[23, 2],
+	[1024, 31]
+];
+
+function run(input) {
+	if (input === 1) return 0;
+	const ring = Math.ceil((Math.sqrt(input) + 1) / 2) - 1;
+	const corner = (2*ring+1)*(2*ring+1);
+	const lateralDistance = Math.abs((corner - input) % (ring * 2) - ring);
+	return ring + lateralDistance;
+}
+
+for (let [input, output] of tests) {
+	assert.strictEqual(run(input), output);
+}
+console.log(run(input));
---
+++ 2017/03b.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const fs = require('fs');
+const input = +fs.readFileSync('03.txt', 'utf-8').trim();
+
+function run(input) {
+	const values = new Map();
+	values.set(`0,0`, 1);
+	for (let ring = 1; ; ++ring) {
+		for (let y = -ring + 1; y <= ring; ++y) {
+			const x = ring;
+			const value = calculateValue(x, y);
+			if (value > input) return value;
+			values.set(`${x},${y}`, value);
+		}
+		for (let x = ring - 1; x >= -ring; --x) {
+			const y = ring;
+			const value = calculateValue(x, y);
+			if (value > input) return value;
+			values.set(`${x},${y}`, value);
+		}
+		for (let y = ring - 1; y >= -ring; --y) {
+			const x = -ring;
+			const value = calculateValue(x, y);
+			if (value > input) return value;
+			values.set(`${x},${y}`, value);
+		}
+		for (let x = -ring + 1; x <= ring; ++x) {
+			const y = -ring;
+			const value = calculateValue(x, y);
+			if (value > input) return value;
+			values.set(`${x},${y}`, value);
+		}
+	}
+
+	function calculateValue(x, y) {
+		return (
+			(values.get(`${x-1},${y-1}`) || 0) +
+			(values.get(`${x  },${y-1}`) || 0) +
+			(values.get(`${x+1},${y-1}`) || 0) +
+			(values.get(`${x+1},${y  }`) || 0) +
+			(values.get(`${x+1},${y+1}`) || 0) +
+			(values.get(`${x  },${y+1}`) || 0) +
+			(values.get(`${x-1},${y+1}`) || 0) +
+			(values.get(`${x-1},${y  }`) || 0)
+		);
+	}
+}
+
+console.log(run(input));
