2cb134d4e0a714ec3765e0f0e2769c999a5bfbfc
Greg Smith <smr.kyu@gmail.com>
2019-12-05 17:24:27 -0600

    2017 Day 5

---
+++ 2017/05b.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync('05.txt', 'utf-8').trim();
+
+function run(input) {
+	const mem = input.split('\n').map(x => +x);
+	let pc = 0;
+	let steps = 0;
+	while (pc >= 0 && pc < mem.length) {
+		++steps;
+		const newPc = pc + mem[pc];
+		if (mem[pc] >= 3) {
+			--mem[pc];
+		} else {
+			++mem[pc];
+		}
+		pc = newPc;
+	}
+	return steps;
+}
+
+console.log(run(input));
