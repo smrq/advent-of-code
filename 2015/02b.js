8fe60b8be31ce1bd06162ef353b21954450702f6
Greg Smith <smr.kyu@gmail.com>
2019-12-06 11:28:41 -0600

    2015 Day 2

---
+++ 2015/02b.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('02.txt', 'utf-8').trim();
+
+function run(input) {
+	return input.split('\n').map(line => {
+		const [l, w, h] = line.split('x').map(n => +n);
+
+		return 2 * (l+w+h-Math.max(l, w, h)) + l*w*h;
+	}).reduce((a, b) => a + b);
+}
+
+console.log(run(input));
