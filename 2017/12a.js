9a624278f41f9a258a4dd4aaa2bca12185db7cb2
Greg Smith <smr.kyu@gmail.com>
2019-12-06 13:34:11 -0600

    2017 Day 12

---
+++ 2017/12a.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const fs = require('fs');
+const input = fs.readFileSync('12.txt', 'utf-8').trim();
+const tests = [
+	['0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5', 6],
+];
+
+function parseInput(input) {
+	const result = new Map();
+	for (let [node, connected] of input.split('\n').map(parseLine)) {
+		result.set(node, { connected });
+	}
+	return result;
+}
+
+function parseLine(line) {
+	let [node, connected] = line.split(' <-> ');
+	node = +node;
+	connected = connected.split(', ').map(x => +x);
+	return [node, connected];
+}
+
+function run(input) {
+	const nodes = parseInput(input);
+	let score = 0;
+	let openSet = [0];
+	while (openSet.length) {
+		const current = nodes.get(openSet.pop());
+		if (!current.visited) {
+			current.visited = true;
+			openSet = openSet.concat(current.connected);
+			++score;
+		}
+	}
+	return score;
+}
+
+for (let [input, output] of tests) {
+	assert.strictEqual(run(input), output);
+}
+console.log(run(input));
