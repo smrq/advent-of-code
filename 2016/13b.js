8078d687ce1a5452b7b5ebc8a46fe076e5519f3c
Greg Smith <smr.kyu@gmail.com>
2019-12-16 19:28:18 -0600

    2016 Day 13

---
+++ 2016/13b.js
@@ -0,0 +1,74 @@
+const assert = require('assert');
+const chalk = require('chalk');
+const fs = require('fs');
+const input = [fs.readFileSync('13.txt', 'utf-8').trim(), 31, 39];
+const tests = [];
+
+function isWall(input, x, y) {
+	const n = x*x + 3*x + 2*x*y + y + y*y + input;
+	return popcount(n) % 2 === 1;
+}
+
+function popcount(n) {
+	let result = 0;
+	while (n) {
+		if (n & 1) ++result;
+		n = n >> 1;
+	}
+	return result;
+}
+
+function key(state) {
+	return state.join(',');
+}
+
+function neighbors(input, x, y) {
+	const result = [];
+	if (x > 0 && !isWall(input, x-1, y)) {
+		result.push([x-1, y]);
+	}
+	if (y > 0 && !isWall(input, x, y-1)) {
+		result.push([x, y-1]);
+	}
+	if (!isWall(input, x+1, y)) {
+		result.push([x+1, y]);
+	}
+	if (!isWall(input, x, y+1)) {
+		result.push([x, y+1]);
+	}
+	return result;
+}
+
+function memo(fn) {
+	const cache = new Map();
+	return (...args) => {
+		const key = args.join('|');
+		if (!cache.has(key)) {
+			cache.set(key, fn(...args));
+		}
+		return cache.get(key);
+	}
+}
+
+function run([input, ...goal]) {
+	input = +input;
+
+	const start = [1, 1];
+	const visited = new Set(['1,1']);
+
+	let openSet = [[1, 1]];
+	for (let i = 0; i < 50; ++i) {
+		openSet = [].concat(...openSet.map(([x, y]) => neighbors(input, x, y)));
+		openSet = openSet.filter(coords => !visited.has(key(coords)));
+		for (let coords of openSet) {
+			visited.add(key(coords));
+		}
+	}
+
+	return visited.size;
+}
+
+for (let [input, output] of tests) {
+	assert.strictEqual(run(input), output, input);
+}
+console.log(run(input));
