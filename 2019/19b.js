4612ac0d317f4b7578500280ab3257a41f65d163
Greg Smith <smr.kyu@gmail.com>
2019-12-19 22:33:37 -0600

    Update 2019 Day 19 Intcode machine

--- 2019/19b.js
+++ 2019/19b.js
@@ -6,31 +6,32 @@ const inputFilename = path.resolve(__dirname, parseInt(path.basename(process.arg
 const input = fs.readFileSync(inputFilename, 'utf-8').trim();
 const tests = [];
 
-async function run(input) {
-	const mem = input.split(',').map(x => parseInt(x, 10));
-	
-	for (let x = 0, y = 1000; ; ++y) {
-		while (await tryCoords(mem, x, y+99) !== 1) {
-			++x;
-		}
-		if (await tryCoords(mem, x+99, y) === 1) {
-			return x * 10000 + y;
-		}
-	}
-}
+let machineIdNumber = 0;
 
-async function tryCoords(mem, x, y) {
+function constructMachine(id, mem) {
 	const stdin = createPipe();
 	const stdout = createPipe();
-	const machine = constructMachine(
-		'Beam',
-		mem,
-		stdin.read,
-		stdout.write);
-	runMachine(machine, false);
-	stdin.write(x);
-	stdin.write(y);
-	return stdout.read();
+	return {
+		baseId: id,
+		id: id + (machineIdNumber++),
+		mem: mem.slice(),
+		stdin,
+		stdout,
+		pc: 0,
+		relativeBase: 0
+	};
+}
+
+function copyMachine(machine) {
+	const stdin = createPipe();
+	const stdout = createPipe();
+	return {
+		...machine,
+		id: machine.baseId + (machineIdNumber++),
+		mem: machine.mem.slice(),
+		stdin,
+		stdout
+	}
 }
 
 function createPipe() {
@@ -59,155 +60,191 @@ function createPipe() {
 	}
 }
 
-function constructMachine(id, mem, getInput, sendOutput) {
-	return {
-		id,
-		mem: mem.slice(),
-		getInput,
-		sendOutput,
-		pc: 0,
-		relativeBase: 0
-	};
+function connectPipes(inPipe, outPipe) {
+	(async function () {
+		for (;;) {
+			await outPipe.write(await inPipe.read());
+		}
+	})();
 }
 
-async function runMachine(machine, debug) {
-	while (await stepMachine(machine, debug)) {}
-}
-
-async function stepMachine(machine, debug) {
-	if (machine.pc == null) {
-		return false;
-	} else {
-		machine.pc = await runInstruction(machine, debug);
-		return true;
-	}
-}
-
-async function runInstruction(machine, debug) {
-	const { id, mem, pc, getInput, sendOutput } = machine;
+function fetchInstruction(mem, pc) {
 	const [z, a, b, c] = mem.slice(pc);
 	const opcode = z % 100;
 	const aMode = (z / 100 | 0) % 10;
 	const bMode = (z / 1000 | 0) % 10;
 	const cMode = (z / 10000 | 0) % 10;
 
-	switch (opcode) {
-		case 1: // c := a + b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b},${c}`);
-				console.log(`[${id}]        ${showAssign(c, cMode)} = ${showArg(a, aMode)} + ${showArg(b, bMode)}`);
-			}
-			assign(c, cMode, value(a, aMode) + value(b, bMode));
-			return pc + 4;
+	return {
+		opcode,
+		a: { n: a, mode: aMode },
+		b: { n: b, mode: bMode },
+		c: { n: c, mode: cMode },
+	};
+}
 
-		case 2: // c := a * b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b},${c}`);
-				console.log(`[${id}]        ${showAssign(c, cMode)} = ${showArg(a, aMode)} * ${showArg(b, bMode)}`);
-			}
-			assign(c, cMode, value(a, aMode) * value(b, bMode));
-			return pc + 4;
+async function runMachineAsync(machine, debug) {
+	const gen = runMachineGen(machine, debug);
+	let result;
+	let value;
+	for (;;) {
+		result = gen.next(value);
+		if (result.done) break;
 
-		case 3: // a := input
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a}`);
-				console.log(`[${id}]        awaiting input...`);
-			}
-			const input = await getInput();
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a}`);
-				console.log(`[${id}]        ${showAssign(a, aMode)} = input <${input}>`);
-			}
-			assign(a, aMode, input);
-			return pc + 2;
+		value = null;
+		if (result.value == null) {
+			value = await machine.stdin.read();
+		} else {
+			await machine.stdout.write(result.value);
+		}
+	}
+}
 
-		case 4: // output a
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a}`);
-				console.log(`[${id}]        output ${showArg(a, aMode)}`);
-			}
-			await sendOutput(value(a, aMode));
-			return pc + 2;
+function *runMachineGen(machine, debug) {
+	while (machine.pc != null) {
+		const { opcode, a, b, c } = fetchInstruction(machine.mem, machine.pc);
 
-		case 5: // jnz a => b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b}`);
-				console.log(`[${id}]        jnz ${showArg(a, aMode)} => ${showArg(b, bMode)}`);
-			}
-			if (value(a, aMode) !== 0) {
-				return value(b, bMode);
-			} else {
-				return pc + 3;
-			}
+		switch (opcode) {
+			case 1: // c := a + b
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 4).padEnd(28, ' ')} ${showAssign(c)} = ${showArg(a)} + ${showArg(b)}`);
+				assign(c, value(a) + value(b));
+				machine.pc += 4;
+				break;
 
-		case 6: // jez a => b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b}`);
-				console.log(`[${id}]        jez ${showArg(a, aMode)} => ${showArg(b, bMode)}`);
-			}
-			if (value(a, aMode) === 0) {
-				return value(b, bMode);
-			} else {
-				return pc + 3;
-			}
+			case 2: // c := a * b
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 4).padEnd(28, ' ')} ${showAssign(c)} = ${showArg(a)} * ${showArg(b)}`);
+				assign(c, value(a) * value(b));
+				machine.pc += 4;
+				break;
 
-		case 7: // c := a < b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b},${c}`);
-				console.log(`[${id}]        ${showAssign(c, cMode)} = ${showArg(a, aMode)} < ${showArg(b, bMode)}`);
-			}
-			assign(c, cMode, value(a, aMode) < value(b, bMode) ? 1 : 0);
-			return pc + 4;
+			case 3: // a := input
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 2).padEnd(28, ' ')} awaiting input...`);
+				const input = yield;
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 2).padEnd(28, ' ')} ${showAssign(a)} = input <${input}>`);
+				assign(a, input);
+				machine.pc += 2;
+				break;
 
-		case 8: // c := a == b
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a},${b},${c}`);
-				console.log(`[${id}]        ${showAssign(c, cMode)} = ${showArg(a, aMode)} == ${showArg(b, bMode)}`);
-			}
-			assign(c, cMode, value(a, aMode) === value(b, bMode) ? 1 : 0);
-			return pc + 4;
+			case 4: // output a
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 2).padEnd(28, ' ')} output ${showArg(a)}`);
+				const output = value(a);
+				machine.pc += 2;
+				yield output;
+				break;
 
-		case 9: // relativeBase += a
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z},${a}`);
-				console.log(`[${id}]        relative base <${machine.relativeBase}> += ${showArg(a, aMode)}`);
-			}
-			machine.relativeBase += value(a, aMode);
-			return pc + 2;
+			case 5: // jnz a => b
+				if (debug) {
+					if (a.mode === 1) {
+						if (a.value === 0) {
+							console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} nop (jnz ${showArg(a)})`);
+						} else {
+							console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} jmp ${showArg(b)} (jnz ${showArg(a)})`);
+						}
+					} else {
+						console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} jnz ${showArg(a)} => ${showArg(b)}`);
+					}
+				}
+				if (value(a) !== 0) {
+					machine.pc = value(b);
+				} else {
+					machine.pc += 3;
+				}
+				break;
 
-		case 99:
-			if (debug) {
-				console.log(`[${id}] ${String(pc).padStart(4)}:  ${z}`);
-				console.log(`[${id}]        halt`);
-			}
-			return null;
+			case 6: // jez a => b
+				if (debug) {
+					if (a.mode === 1) {
+						if (a.value === 0) {
+							console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} jmp ${showArg(b)} (jez ${showArg(a)})`);
+						} else {
+							console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} nop (jnz ${showArg(a)})`);
+						}
+					} else {
+						console.log(`[${machine.id}] ${showMem(machine.pc, 3).padEnd(28, ' ')} jez ${showArg(a)} => ${showArg(b)}`);
+					}
+				}
+				if (value(a) === 0) {
+					machine.pc = value(b);
+				} else {
+					machine.pc += 3;
+				}
+				break;
+
+			case 7: // c := a < b
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 4).padEnd(28, ' ')} ${showAssign(c)} = ${showArg(a)} < ${showArg(b)}`);
+				assign(c, value(a) < value(b) ? 1 : 0);
+				machine.pc += 4;
+				break;
+
+			case 8: // c := a == b
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 4).padEnd(28, ' ')} ${showAssign(c)} = ${showArg(a)} == ${showArg(b)}`);
+				assign(c, value(a) === value(b) ? 1 : 0);
+				machine.pc += 4;
+				break;
+
+			case 9: // relativeBase += a
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 2).padEnd(28, ' ')} relative base <${machine.relativeBase}> += ${showArg(a)}`);
+				machine.relativeBase += value(a);
+				machine.pc += 2;
+				break;
+
+			case 99:
+				if (debug) console.log(`[${machine.id}] ${showMem(machine.pc, 1).padEnd(28, ' ')} halt`);
+				machine.pc = null;
+				break;
+		}
 	}
 
-	function value(n, mode) {
-		if (mode === 0) { return machine.mem[n] || 0; }
-		if (mode === 1) { return n; }
-		if (mode === 2) { return machine.mem[machine.relativeBase + n] || 0; }
-		throw new Error(`invalid mode ${JSON.stringify(mode)} on rhs`);
+	function value(arg) {
+		if (arg.mode === 0) { return machine.mem[arg.n] || 0; }
+		if (arg.mode === 1) { return arg.n; }
+		if (arg.mode === 2) { return machine.mem[machine.relativeBase + arg.n] || 0; }
+		throw new Error(`invalid mode ${JSON.stringify(arg.mode)} on rhs`);
 	}
 
-	function assign(n, mode, value) {
-		if (mode === 0) { machine.mem[n] = value; return; }
-		if (mode === 2) { machine.mem[machine.relativeBase + n] = value; return; }
+	function assign(arg, value) {
+		if (arg.mode === 0) { machine.mem[arg.n] = value; return; }
+		if (arg.mode === 2) { machine.mem[machine.relativeBase + arg.n] = value; return; }
 		throw new Error(`invalid mode ${JSON.stringify(mode)} on lhs`);
 	}
 
-	function showArg(n, mode) {
-		if (mode === 0) { return `[${n}]<${machine.mem[n] || 0}>`; }
-		if (mode === 1) { return `${n}`; }
-		if (mode === 2) { return `[${machine.relativeBase}+${n}]<${machine.mem[machine.relativeBase+n] || 0}>`; }
+	function showMem(addr, len) {
+		return `${String(addr).padStart(4)}:  ${machine.mem.slice(addr, addr+len).join(',')}`;
+	}
+
+	function showArg(arg) {
+		if (arg.mode === 0) { return `[${arg.n}]<${value(arg)}>`; }
+		if (arg.mode === 1) { return String(value(arg)); }
+		if (arg.mode === 2) { return `[${machine.relativeBase}+${arg.n}]<${value(arg)}>`; }
 		throw new Error();
 	}
 
-	function showAssign(n, mode) {
-		if (mode === 0) { return `[${n}]`; }
-		if (mode === 2) { return `[${machine.relativeBase}+${n}]`; }
+	function showAssign(arg) {
+		if (arg.mode === 0) { return `[${arg.n}]`; }
+		if (arg.mode === 2) { return `[${machine.relativeBase}+${arg.n}]`; }
 		throw new Error();
 	}
+}
+
+async function run(input) {
+	const mem = input.split(',').map(x => parseInt(x, 10));
+	
+	for (let x = 0, y = 1000; ; ++y) {
+		while (await tryCoords(mem, x, y+99) !== 1) {
+			++x;
+		}
+		if (await tryCoords(mem, x+99, y) === 1) {
+			return x * 10000 + y;
+		}
+	}
+}
+
+async function tryCoords(mem, x, y) {
+	const machine = constructMachine('Beam', mem);
+	runMachineAsync(machine, false);
+	machine.stdin.write(x);
+	machine.stdin.write(y);
+	return machine.stdout.read();
 }
 
 (async function () {
