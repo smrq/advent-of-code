e03fdb0a2d01af475d5062270eb3bd232ea6ac3c
Greg Smith <smr.kyu@gmail.com>
2020-12-06 23:24:03 -0600

    2020 Day 7

---
+++ 2020/07a.js
@@ -0,0 +1,39 @@
+const { getRawInput } = require('../lib');
+
+const rawInput = getRawInput();
+const input = parseInput(rawInput);
+
+console.log(run(input));
+
+function run(input) {
+	const open = ['shiny gold'];
+	const result = new Set();
+
+	while (open.length) {
+		const color = open.pop();
+		const containers = input.filter(map => map.inner.some(i => i.color === color));
+		for (let container of containers) {
+			if (!result.has(container.outer)) {
+				result.add(container.outer);
+				open.push(container.outer);
+			}
+		}
+	}
+
+	return result.size;
+}
+
+function parseInput(str) {
+	return str.split('\n').map(line => {
+		let [outer, inner] = line.split(' bags contain ');
+		if (inner === 'no other bags.') {
+			return { outer, inner: [] };
+		}
+
+		inner = inner.split(', ').map(x => {
+			let [_, n, color] = /(\d+) ([\w ]+) bags?/.exec(x);
+			return { n: +n, color };
+		});
+		return { outer, inner };
+	});
+}
