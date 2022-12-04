dc0e9783f4bea331d3ec89e259ebded33d9f9ca3
Greg Smith <smrq@smrq.net>
2022-12-04 02:53:21 -0600

    2022 Day 4

---
+++ 2022/04a.mjs
@@ -0,0 +1,25 @@
+import * as L from '../lib.mjs';
+
+L.runTests(args => run(args), [
+	parseInput(`2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`), 2
+]);
+
+const input = parseInput(L.getRawInput());
+console.log(run(input));
+
+function run(input) {
+	return input.filter(([a, b]) => isContained(a, b) || isContained(b, a)).length;
+}
+
+function parseInput(str) {
+	return L.autoparse(str).map(line => line.split(',').map(x => x.split('-').map(n => parseInt(n, 10))));
+}
+
+function isContained([a0, a1], [b0, b1]) {
+	return a0 <= b0 && a1 >= b1;
+}
