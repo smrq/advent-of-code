4f1b9b1eeb41674df0c86c1910f6333ba12cf3eb
Greg Smith <smrq@smrq.net>
2024-12-11 13:08:54 -0600

    2024 Day 8

---
+++ 2024/08a.mjs
@@ -0,0 +1,50 @@
+import * as L from '../lib.mjs';
+
+L.runTests(args => run(args), [
+	parseInput(`............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`), 14
+]);
+
+const input = parseInput(L.getRawInput());
+console.log(run(input));
+
+function run(input) {
+	const frequencies = new Set(input.flat());
+	frequencies.delete('.');
+
+	let locations = new Set();
+
+	for (let frequency of frequencies) {
+		const antennas = L.findAllIndices2d(input, x => x === frequency);
+		for (let i = 0; i < antennas.length; ++i) {
+			for (let j = i + 1; j < antennas.length; ++j) {
+				let dy = antennas[i][0] - antennas[j][0];
+				let dx = antennas[i][1] - antennas[j][1];
+
+				for (let n of [1, -2]) {
+					const y = antennas[i][0] + n * dy;
+					const x = antennas[i][1] + n * dx;
+					if (L.inBounds(input, y, x)) {
+						locations.add(`${y},${x}`);
+					}
+				}
+			}
+		}
+	}
+
+	return locations.size;
+}
+
+function parseInput(str) {
+	return L.autoparse(str);
+}
